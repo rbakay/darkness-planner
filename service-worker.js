553b9ca5bdb6bc1acb781f3309419bc776341fec
rbakay <33573515+rbakay@users.noreply.github.com>
2025-12-04 21:58:40 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // -------------------------------
 // Service Worker for Darkness Planner
 // Increase this version string to force an update:
-const CACHE_NAME = 'darkness-planner-v1.40';
+const CACHE_NAME = 'darkness-planner-v1.41';
 // -------------------------------
 
 const ASSETS = [
