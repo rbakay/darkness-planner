6688e36a1671d5f6322fe4cc087bfda5e8bf7a21
rbakay <33573515+rbakay@users.noreply.github.com>
2025-12-04 22:14:07 -0500

    Update lang-ru.js

--- lang-ru.js
+++ lang-ru.js
@@ -84,5 +84,5 @@ window.DARK_LANG_RU = {
   geoFailed: 'Не удалось получить координаты.',
   filterAstrStart: 'Начало астрономической ночи',
   filterAstrEnd: 'Конец астрономической ночи',
-  versionLabel: 'Darkness Planner · v1.41'
+  versionLabel: 'Darkness Planner · v1.42'
 };
